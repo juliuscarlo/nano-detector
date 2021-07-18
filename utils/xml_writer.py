602719380917caf77b5e2ecd83f5cd8102ec7741
Julius Nick <julius.nick@gmail.com>
2021-07-18 21:41:41 +0200

    fixed xml output to correct schema

--- utils/xml_writer.py
+++ utils/xml_writer.py
@@ -10,7 +10,7 @@ Author: Julius Nick (julius.nick@gmail.com)
 import xml.etree.cElementTree as etree
 
 
-def create_tree(img_shape, object_list, filename="filename.pic", date="01.01.1999"):
+def create_tree(img_shape, object_list, filename, date):
     """Creates an element tree structure, which can easily be exported to an XML
     file.
 
@@ -20,32 +20,30 @@ def create_tree(img_shape, object_list, filename="filename.pic", date="01.01.199
         filename: the filename of the image
         date: the date on which the image is annotated (current date)
     """
-    root = etree.Element("xs:schema", {"targetNamespace": "http://www.fernuni-hagen.de/gmaf",
-                                       "elementFormDefault": "qualified", "xmlns": "gmaf_schema.xsd",
-                                       "xmlns:xs": "http://www.w3.org/2001/XMLSchema"})
+    root = etree.Element("gmaf-collection", {"xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+                                             "xsi:noNamespaceSchemaLocation": "gmaf_schema.xsd"})
 
-    collection = etree.SubElement(root, "xs:gmaf-collection")
-    gmaf_data = etree.SubElement(collection, "xs:gmaf-data")
+    gmaf_data = etree.SubElement(root, "gmaf-data")
 
-    etree.SubElement(gmaf_data, "xs:file").text = filename
-    etree.SubElement(gmaf_data, "xs:date").text = date
-    objects = etree.SubElement(gmaf_data, "xs:objects")
+    etree.SubElement(gmaf_data, "file").text = filename
+    etree.SubElement(gmaf_data, "date").text = date
+    objects = etree.SubElement(gmaf_data, "objects")
 
     for item in object_list:
-        object = etree.SubElement(objects, "xs:object")
+        object = etree.SubElement(objects, "object")
 
-        etree.SubElement(object, "xs:term").text = item["term"]
-        bounding_box = etree.SubElement(object, "xs:bounding-box")
+        etree.SubElement(object, "term").text = item["term"]
+        bounding_box = etree.SubElement(object, "bounding-box")
 
         print(item["loc"])
         box = convert_box(img_shape, relative_box=item["loc"])
 
-        etree.SubElement(bounding_box, "xs:x").text = str(box[0])
-        etree.SubElement(bounding_box, "xs:y").text = str(box[1])
-        etree.SubElement(bounding_box, "xs:width").text = str(box[2])
-        etree.SubElement(bounding_box, "xs:height").text = str(box[3])
+        etree.SubElement(bounding_box, "x").text = str(box[0])
+        etree.SubElement(bounding_box, "y").text = str(box[1])
+        etree.SubElement(bounding_box, "width").text = str(box[2])
+        etree.SubElement(bounding_box, "height").text = str(box[3])
 
-        etree.SubElement(object, "xs:probability").text = str(
+        etree.SubElement(object, "probability").text = str(
             item["probability"])
 
     tree = etree.ElementTree(root)
